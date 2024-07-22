8610f2707ba1294c95c9b5dfbda7240ddb796653
juliaret <julia.retuertanogales@alumnostrazos.net>
2024-07-22 17:21:29 +0200

    practica-casa-22-julio

--- src/js/index.js
+++ src/js/index.js
@@ -20,6 +20,12 @@ const cardExpDateYYElement = document.getElementById('card-exp-date-yy');
 const cardholderCvcInputElement = document.getElementById('cardholder-cvc');
 const cardCvcElement = document.getElementById('card-cvc');
 
+const errorElements = document.querySelectorAll('.blank-error');
+
+const formElement = document.getElementById('form');
+
+const confirmationMessageElement = document.getElementById('confirmation-message');
+
 const fillName = () => {
   cardNameElement.textContent = cardholderNameInputElement.value;
   if (cardholderNameInputElement.value === '') {
@@ -29,18 +35,30 @@ const fillName = () => {
 
 const fillNumber = () => {
   cardNumberElement.textContent = cardholderNumberInputElement.value;
+  if (cardholderNumberInputElement.value === '') {
+    cardNumberElement.textContent = '0000 0000 0000 0000';
+  }
 };
 
 const fillExpDateMonth = () => {
   cardExpDateMMElement.textContent = cardholderExpDateMMInputElement.value;
+  if (cardholderExpDateMMInputElement.value === '') {
+    cardExpDateMMElement.textContent = '00';
+  }
 };
 
 const fillExpDateYear = () => {
   cardExpDateYYElement.textContent = cardholderExpDateYYInputElement.value;
+  if (cardholderExpDateYYInputElement.value === '') {
+    cardExpDateYYElement.textContent = '00';
+  }
 };
 
 const fillCvc = () => {
   cardCvcElement.textContent = cardholderCvcInputElement.value;
+  if (cardholderCvcInputElement.value === '') {
+    cardCvcElement.textContent = '000';
+  }
 };
 
 cardholderNameInputElement.addEventListener('input', fillName);
@@ -49,30 +67,104 @@ cardholderExpDateMMInputElement.addEventListener('input', fillExpDateMonth);
 cardholderExpDateYYInputElement.addEventListener('input', fillExpDateYear);
 cardholderCvcInputElement.addEventListener('input', fillCvc);
 
+// Validaciones
+
+const showError = (index, message) => {
+  errorElements[index].textContent = message;
+};
+
+const clearError = index => {
+  errorElements[index].textContent = '';
+};
+
+const validateInputs = () => {
+  // variable para comprobar si los inputs son válidos
+  let isValid = true;
+
+  // Validar nombre
+  if (cardholderNameInputElement.value.length === 0) {
+    showError(0, "Can't be blank");
+    isValid = false;
+  } else {
+    clearError(0);
+  }
+
+  // Validar número de tarjeta
+  if (cardholderNumberInputElement.value.length === 0) {
+    showError(1, "Can't be blank");
+    isValid = false;
+  } else if (cardholderNumberInputElement.value.length !== 16) {
+    showError(1, 'Number is too short');
+    isValid = false;
+  } else {
+    clearError(1);
+  }
+
+  // Validar fecha de expiración
+  // MM
+  if (cardholderExpDateMMInputElement.value.length === 0) {
+    showError(2, "Can't be blank");
+    isValid = false;
+  } else {
+    clearError(2);
+  }
+
+  // YY
+  if (cardholderExpDateYYInputElement.value.length === 0) {
+    showError(2, "Can't be blank");
+    isValid = false;
+  } else {
+    clearError(2);
+  }
+
+  // Validar CVC
+  if (cardholderCvcInputElement.value.length === 0) {
+    showError(3, "Can't be blank");
+    isValid = false;
+  } else if (cardholderCvcInputElement.value.length !== 3) {
+    showError(3, 'Number is too short');
+    isValid = false;
+  } else {
+    clearError(3);
+  }
+
+  // si todos los campos son válidos
+  return isValid;
+};
+
+formElement.addEventListener('submit', event => {
+  event.preventDefault();
+  const isValid = validateInputs();
+
+  // si la inf está bien
+  if (isValid) {
+    formElement.classList.add('hidden');
+    confirmationMessageElement.classList.remove('hidden');
+  }
+});
+
 // IMPRIMIR ERRORES
 
-const formElement = document.querySelectorAll('form');
+// const defaultValues = {
+//   name: 'JANE APPLESEED',
+//   number: '0000 0000 0000 0000',
+//   month: '00',
+//   year: '00',
+//   cvc: '000'
+// };
 
-const defaultValues = {
-  name: 'JANE APPLESEED',
-  number: '0000 0000 0000 0000',
-  month: '00',
-  year: '00',
-  cvc: '000'
-};
+// const writeInCard = event => {
+//   const textCard = document.getElementById(`card-${event.target.id}`);
+//   const onlyNumbers = '0123456789';
+//   if (event.target.id === 'name') {
+//     const value = event.target.value;
+//     let isCorrect = true;
+//     for (const letter of value) {
+//       if (onlyNumbers.includes(letter)) {
+//         isCorrect = false;
+//       }
+//     }
+//   }
+// };
 
-const writeInCard = event => {
-  const textCard = document.getElementById(`card-${event.target.id}`);
-  const onlyNumbers = '0123456789';
-  if (event.target.id === 'name') {
-    const value = event.target.value;
-    let isCorrect = true;
-    for (const letter of value) {
-      if (onlyNumbers.includes(letter)) {
-        isCorrect = false;
-      }
-    }
-  }
-};
-
-formElement.addEventListener('input', writeInCard);
+// formElement.addEventListener('input', writeInCard);
